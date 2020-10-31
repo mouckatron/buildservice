3f2e201508e0b87e12e8f12071ea3e30251518d7
Graham Moucka <mouckatron@gmail.com>
2020-10-31 10:00:24 +0000

    A looping service with signals

--- cmd/buildservice/main.go
+++ cmd/buildservice/main.go
@@ -1,6 +1,42 @@
 package main
 
-import ()
+import (
+	"log"
+	"math/rand"
+	"os"
+	"os/signal"
+	"time"
+)
 
 func main() {
+	setupSignals()
+}
+
+func setupSignals() {
+	sigs := make(chan os.Signal, 1)
+
+	// catch all signals since not explicitly listing
+	signal.Notify(sigs)
+	//signal.Notify(sigs,syscall.SIGQUIT)
+
+	// method invoked upon seeing signal
+	go func() {
+		s := <-sigs
+		log.Printf("RECEIVED SIGNAL: %s", s)
+		appCleanup()
+		os.Exit(1)
+	}()
+
+	for {
+		log.Printf("hello, world!")
+
+		// wait random number of milliseconds
+		Nsecs := rand.Intn(3000)
+		log.Printf("About to sleep %dms before looping again", Nsecs)
+		time.Sleep(time.Millisecond * time.Duration(Nsecs))
+	}
+
+}
+
+func appCleanup() {
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module moucka.cloud/buildservice/v2
 
 go 1.15
 
-require gopkg.in/yaml.v2 v2.3.0
+require (
+	golang.org/x/tools v0.0.0-20201030204249-4fc0492b8eca // indirect
+	golang.org/x/tools/gopls v0.5.2 // indirect
+	gopkg.in/yaml.v2 v2.3.0
+)
