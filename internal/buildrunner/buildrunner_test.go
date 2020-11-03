ad90ba150828cfbd37e90140d803fef9eb6b472d
Graham Moucka <mouckatron@gmail.com>
2020-11-03 23:36:57 +0000

    A little more test coverage

--- internal/buildrunner/buildrunner_test.go
+++ internal/buildrunner/buildrunner_test.go
@@ -1,6 +1,86 @@
 package buildrunner
 
-import "testing"
+import (
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
+)
+
+func cleanupAfterSetup(b *BuildSettings) {
+	b.LogFile.Close()
+	os.RemoveAll("/tmp/buildservice")
+}
+
+func TestSetupName(t *testing.T) {
+	b := BuildSettings{URL: "github.com/mouckatron/buildservice.git"}
+
+	expected := "buildservice"
+
+	homeDir = "/tmp"
+	buildsDir = "/tmp"
+	Setup(&b)
+
+	if b.Name != expected {
+		t.Errorf("Bad Name: wanted %s, got %s", expected, b.Name)
+	}
+
+	cleanupAfterSetup(&b)
+}
+
+// This seems a bit pointless...but in the name of coverage
+func TestSetupID(t *testing.T) {
+	b := BuildSettings{}
+
+	now := time.Now()
+	expected := now.Format("20060102-150405")
+
+	homeDir = "/tmp"
+	buildsDir = "/tmp"
+	Setup(&b)
+
+	if b.ID != expected {
+		t.Errorf("Bad ID: wanted %s, got %s (this may also be a timing issue if it rolled over the second)",
+			expected, b.ID)
+	}
+
+	cleanupAfterSetup(&b)
+}
+
+func TestSetupWorkDirectory(t *testing.T) {
+	b := BuildSettings{
+		Name: "buildservice"}
+
+	wdExpected := "/tmp/buildservice"
+
+	homeDir = "/tmp"
+	buildsDir = "/tmp"
+	Setup(&b)
+
+	if !strings.HasPrefix(b.WorkingDir, wdExpected) {
+		t.Errorf("Working directory bad: expected %s, got %s", wdExpected, b.WorkingDir)
+	}
+
+	cleanupAfterSetup(&b)
+}
+
+func TestSetupLogging(t *testing.T) {
+	logFilename := "build.log"
+	b := BuildSettings{
+		Name: "buildservice"}
+
+	homeDir = "/tmp"
+	buildsDir = "/tmp"
+
+	Setup(&b)
+
+	if _, err := os.Stat(filepath.Join(b.WorkingDir, logFilename)); err != nil {
+		t.Errorf("%s did not exist", logFilename)
+	}
+
+	cleanupAfterSetup(&b)
+}
 
 func TestExecCmdMultiple(t *testing.T) {
 	var tests = []struct {
