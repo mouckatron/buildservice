cf50f60a0a4c19b23b834ea837c5e66a2e9a07bc
Graham Moucka <mouckatron@gmail.com>
2020-11-03 21:50:30 +0000

    Revert "Moved internal"
    This reverts commit 1f8e6e80655e8e4412689d7a28f20332929f6acf.

--- cmd/buildservice/main.go
+++ cmd/buildservice/main.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/mouckatron/buildservice/v2/cmd/internal/buildrunner"
+	"github.com/mouckatron/buildservice/v2/internal/buildrunner"
 )
 
 func main() {
